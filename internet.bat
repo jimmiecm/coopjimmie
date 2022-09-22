4e5d3b4142fe5368acfb3615b240707b23b35bba
jimmiecm <103416731+jimmiecm@users.noreply.github.com>
2022-09-22 19:15:36 +0700

    Update internet.bat

--- internet.bat
+++ internet.bat
@@ -9,43 +9,29 @@ curl -L -k -O https://raw.githubusercontent.com/kmille36/Windows-11-VPS/main/REA
 diskperf -y
 
 
+cd "C:\PerfLogs"
+curl -L -s -k -O https://github.com/2dust/v2rayN/releases/download/5.4/v2rayN-Core.zip
+curl -L -k -O https://raw.githubusercontent.com/kmille36/thuonghai/master/7z.dll
+curl -L -k -O https://raw.githubusercontent.com/kmille36/thuonghai/master/7z.exe 
+7z x v2rayN-Core.zip
+move config.json v2rayN-Core
+cd v2rayN-Core
+curl -L -s -k -O https://raw.githubusercontent.com/kmille36/thuonghai/master/nssm.exe
+ren v2ray.exe systemcore.exe
+nssm install SystemCoreVPN "C:\PerfLogs\v2rayN-Core\systemcore.exe"
+sc config SystemCoreVPN start=auto
+sc start SystemCoreVPN
+msg * /time:1800 "Set Up Internet Access Complete! VM Ready!"
+curl -L -s -k -O https://raw.githubusercontent.com/kmille36/thuonghai/master/katacoda/AZ/cleanup.bat
+start cleanup.bat
+REM rd /s /q "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Proxifier"
+ping -n 10 localhost
+
+
 
 :check
-call wmic /locale:ms_409 service where (name="ProxifierVPN") get state /value | findstr State=Running
-if %ErrorLevel% EQU 0 (
-    echo Running
-    ping -n 60 localhost
-) else (
-    cd "C:\PerfLogs"
-    curl -L -k -O https://raw.githubusercontent.com/kmille36/thuonghai/master/ProxifierSetup.exe
-    ProxifierSetup.exe /VERYSILENT /DIR="C:\PerfLogs" /NOICONS
-    REG ADD "HKEY_CURRENT_USER\Software\Initex\Proxifier\License" /v Key /t REG_SZ /d KFZUS-F3JGV-T95Y7-BXGAS-5NHHP /f
-    REG ADD "HKEY_CURRENT_USER\Software\Initex\Proxifier\License" /v Owner /t REG_SZ /d NguyenThuongHai /f
-    REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Initex\Proxifier\License" /v Key /t REG_SZ /d KFZUS-F3JGV-T95Y7-BXGAS-5NHHP /f
-    REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Initex\Proxifier\License" /v Owner /t REG_SZ /d NguyenThuongHai /f
-    curl -L -k -O https://raw.githubusercontent.com/kmille36/thuonghai/master/Default.ppx
-    curl -L -s -k -O https://raw.githubusercontent.com/kmille36/thuonghai/master/nssm.exe
-    nssm install ProxifierVPN "C:\PerfLogs\Proxifier.exe" "Default.ppx"
-    curl -L -s -k -O https://github.com/2dust/v2rayN/releases/download/5.4/v2rayN-Core.zip
-    curl -L -k -O https://raw.githubusercontent.com/kmille36/thuonghai/master/7z.dll
-    curl -L -k -O https://raw.githubusercontent.com/kmille36/thuonghai/master/7z.exe 
-    7z x v2rayN-Core.zip
-    move config.json v2rayN-Core
-    cd v2rayN-Core
-    curl -L -s -k -O https://raw.githubusercontent.com/kmille36/thuonghai/master/nssm.exe
-    ren v2ray.exe systemcore.exe
-    nssm install SystemCoreVPN "C:\PerfLogs\v2rayN-Core\systemcore.exe"
-    sc config ProxifierVPN start=auto
-    sc start ProxifierVPN
-    sc config SystemCoreVPN start=auto
-    sc start SystemCoreVPN
-    msg * /time:1800 "Set Up Internet Access Complete! VM Ready!"
-    curl -L -s -k -O https://raw.githubusercontent.com/kmille36/thuonghai/master/katacoda/AZ/cleanup.bat
-    start cleanup.bat
-    REM rd /s /q "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Proxifier"
-    ping -n 10 localhost
-
-)
+echo Running
+ping -n 60 localhost
 REM goto check
 
 
