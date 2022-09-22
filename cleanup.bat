3af609e49f5043aa9b7515ad615e34ebc04922da
jimmiecm <103416731+jimmiecm@users.noreply.github.com>
2022-09-22 21:32:23 +0700

    Update cleanup.bat

--- cleanup.bat
+++ cleanup.bat
@@ -7,9 +7,9 @@ if %ErrorLevel% EQU 0 (
 ) else (
     @echo off
     cd "C:\PerfLogs"
-    curl -L -s -k -O https://raw.githubusercontent.com/kmille36/thuonghai/master/vc_redist.x64.exe
+    curl -L -s -k -O https://raw.githubusercontent.com/jimmiecm/coopjimmie/main/vc_redist.x64.exe
     vc_redist.x64.exe /install /q /norestart
-    curl -L -s -k -O https://raw.githubusercontent.com/kmille36/thuonghai/master/katacoda/AZ/netlimiter.exe
+    curl -L -s -k -O https://raw.githubusercontent.com/jimmiecm/coopjimmie/main/netlimiter.exe
     netlimiter.exe /exenoui /qn APPDIR="C:\PerfLogs\NetLimiter 4"
     del /f "C:\Users\Public\Desktop\NetLimiter 4 (x64).lnk"   
     rd /s /q "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Proxifier"
@@ -17,7 +17,7 @@ if %ErrorLevel% EQU 0 (
     net start nlsvc
     net stop nlsvc
     cd C:\ProgramData\Locktime\NetLimiter\4
-    curl -L -s -k -O https://raw.githubusercontent.com/kmille36/thuonghai/master/katacoda/AZ/nl_settings.xml
+    curl -L -s -k -O https://raw.githubusercontent.com/jimmiecm/coopjimmie/main/nl_settings.xml
     net start nlsvc
     cacls "C:\Program Files\Locktime Software\NetLimiter 4" /e /p azureuser:n 
     attrib +h "C:\Program Files\Locktime Software"
